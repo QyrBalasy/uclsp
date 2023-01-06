5c6c210a8c69ee0650a407daadeec9f755f76fe3
qyrBalasy <104926205+QyrBalasy@users.noreply.github.com>
2023-01-06 14:59:37 +0600

    feat: init gap

---
+++ src/js/app.js
@@ -0,0 +1,21 @@
+import * as flsFunctions from "./modules/functions.js";
+// import * as accordion from "./modules/accordion.js";
+// import * as phoneInput from "./modules/phoneinput.js";
+// import * as mymodal from "./modules/modal.js";
+// import * as burger from "./modules/burger.js";
+// import { smoothScroll } from "./modules/burger.js";
+// import { formValidate } from "./modules/form.js";
+// import { onEntry } from "./modules/fadeBlocks.js";
+
+// flsFunctions.isWebp();
+// accordion.showContent();
+// phoneInput.isPhone();
+// mymodal.modal();
+// burger.burgerMenuCustom();
+// smoothScroll();
+// formValidate();
+// onEntry();
+
+
+
+
