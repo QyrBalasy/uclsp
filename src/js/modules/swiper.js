62561c440d553a834b8e9cf6d3ad9bcb67cfe85b
qyrBalasy <104926205+QyrBalasy@users.noreply.github.com>
2023-01-27 08:28:26 +0600

    feat: add reviews

--- src/js/modules/swiper.js
+++ src/js/modules/swiper.js
@@ -3,44 +3,26 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
-// export function swiper() {
-//   const swiper = new Swiper(".swiper", {
-//     modules: [Navigation, Pagination],
-//     // Optional parameters
-//     direction: "horizontal",
-//     loop: true,
-
-//     // If we need pagination
-//     pagination: {
-//       el: ".swiper-pagination",
-//     },
-
-//     // Navigation arrows
-//     navigation: {
-//       nextEl: ".swiper-button-next",
-//       prevEl: ".swiper-button-prev",
-//     },
-
-//     // And if we need scrollbar
-//     scrollbar: {
-//       el: ".swiper-scrollbar",
-//     },
-//   });
-// }
 
 export function MySwiper() {
-  const swiper = new Swiper(".swiper", {
+  const swiper = new Swiper(".reviewSwiper", {
     modules: [Navigation, Pagination],
     // Optional parameters
     direction: "horizontal",
     loop: false,
+
     slidesPerView: 3,
-    spaceBetween: 64,
+    mousewheel: true,
+    keyboard: true,
+    // roundLengths: true,
+    // watchOverflow: true,
+    // centeredSlides: true,
 
     // If we need pagination
-    pagination: {
-      el: ".swiper-pagination",
-    },
+    // pagination: {
+    //   el: ".swiper-pagination",
+    //   clickable: true,
+    // },
 
     // Navigation arrows
     navigation: {
@@ -51,6 +33,34 @@ export function MySwiper() {
     // And if we need scrollbar
     scrollbar: {
       el: ".swiper-scrollbar",
+    },
+
+    simulateTouch: false,
+
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+      pageUpDown: true,
+    },
+
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+        centeredSlides: true,
+      },
+      // when window width is >= 480px
+      410: {
+        slidesPerView: 2,
+        spaceBetween: 16,
+      },
+      // when window width is >= 640px
+      750: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+        centeredSlides: false,
+      },
     },
   });
 }
